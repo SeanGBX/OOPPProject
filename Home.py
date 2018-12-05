ee3e76b9e58a74f65a8bccf12b168463b00499a8
SeanGBX <180570C@mymail.nyp.edu.sg>
2018-12-05 13:18:10 +0800

    Updated (1)

--- Home.py
+++ Home.py
@@ -3,13 +3,25 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
-@app.route("/<user>")
-def index(user = None):
-    return render_template("home.html", user=user)
+def overview():
+    return render_template("home.html")
 
 @app.route("/register")
 def register():
     return render_template("register.html")
+
+@app.route("/expiration")
+def expiration():
+    return render_template("expiration.html")
+
+@app.route("/expenditure")
+def expenditure():
+    return render_template("expenditure.html")
+
+@app.route("/profile/<user>")
+def index(user = None):
+    return render_template("profile.html", user=user)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
