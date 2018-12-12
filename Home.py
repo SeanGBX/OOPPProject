e5847069aa166fce97cdda72d178ec4ef3ebb1cd
ZiYi <184116L@mymail.nyp.edu.sg>
2018-12-12 12:52:16 +0700

    Zi Yi Part

--- Home.py
+++ Home.py
@@ -2,21 +2,26 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def overview():
     return render_template("home.html")
+
 
 @app.route("/register")
 def register():
     return render_template("register.html")
 
+
 @app.route("/expiration")
 def expiration():
     return render_template("expiration.html")
 
+
 @app.route("/expenditure")
 def expenditure():
     return render_template("expenditure.html")
+
 
 @app.route("/profile/<user>")
 def index(user = None):
